91edfcc18ef22702bc3794a0646a8a8c1e20d4fb
watchirapong <125724891+watchirapong@users.noreply.github.com>
2025-01-26 10:59:15 +0700

    normal

---
+++ Assets/Script/KillObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class KillerObject : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                PlayerHealth.Instance.RemoveHealth(200f);
+                Debug.Log("Player has been killed by " + gameObject.name);
+            }
+        }
+    }
+}
